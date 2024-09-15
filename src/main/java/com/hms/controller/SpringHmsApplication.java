dcdb7b45568c7a732dac1c37a1392c0a319904a3
Muhammad Mushfiq <mushfiq_csekuet@yahoo.com>
2024-09-15 17:51:55 +0600

    db con

--- src/main/java/com/hms/controller/SpringHmsApplication.java
+++ src/main/java/com/hms/controller/SpringHmsApplication.java
@@ -3,13 +3,25 @@ package com.hms.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class SpringHmsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test compilation for \"Spring HMS\"");
-
+		System.out.println("Test run for \"Spring HMS\"");
 		SpringApplication.run(SpringHmsApplication.class, args);
+
+		try {
+			// The newInstance() call is a work around for some broken Java implementations
+			Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+			//JOptionPane.showMessageDialog(null,"JDBC Driver load success");
+			System.out.println("JDBC Driver loaded successfully");
+		} catch (Exception ex) {
+			// handle the error
+			System.out.println("Problem with JDBC Driver"+ex.toString());
+			//JOptionPane.showMessageDialog(null,JOptionPane.ERROR_MESSAGE+"Problem with JDBC Driver");
+		}
 	}
 
 }
